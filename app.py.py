62b67ead097dfb76a154944eb8d49a6c524eafd1
BIJESH B <arbijesh@gmail.com>
2025-02-11 22:00:42 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -10,7 +10,7 @@ st.title("🚀 Profit Predictor: Enhancing Business Decisions with Data Science"
 st.markdown("### 📊 Enter Order Details to Predict Profit:")
 
 # Load trained Gradient Boosting model
-with open("gb_model_final_hyper.pkl", "rb") as model_file:
+with open("gb_model_final_hyperparameter.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
 # Load LabelEncoders
